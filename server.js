eee998d0113fbb6908f981537d4e3698adfd8ce2
WinnieCWu <winniechn7@gmail.com>
2022-04-04 16:58:01 -0700

    created sessions for log in and log out

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const path = require('path');
-const express = require("express");
-<<<<<<< HEAD
-const routes = require("./controllers/");
-const sequelize = require("./config/connection");
-
-=======
+const express = require('express');
 const session = require('express-session');
->>>>>>> develop
 const exphbs = require('express-handlebars');
 
 const app = express();
@@ -28,19 +22,20 @@ app.use(session(sess));
 
 const hbs = exphbs.create({});
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //static() can take all contents of folder and serve as static assets. 
 //useful for front-end files
 app.use(express.static(path.join(__dirname, 'public')));
 //set as app's preferred template engine of choice
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(require('./controllers/'));
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
